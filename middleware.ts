5b8ed8a0a40d86abb9731b1e3f5288e26bf786bc
codaeddie <eddiecoda24@gmail.com>
2025-10-19 13:34:41 -0700

    Exclude auth from middleware

--- middleware.ts
+++ middleware.ts
@@ -48,8 +48,8 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Skip Next.js internals, static files, and auth pages
+    "/((?!_next|signin|signup|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
     "/(api|trpc)(.*)",
   ],
