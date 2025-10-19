b6044aaaf13a8f76549fc5d64e01bbc3bcd4d83e
codaeddie <eddiecoda24@gmail.com>
2025-10-19 15:57:45 -0700

    hash routing

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -10,6 +10,7 @@ export default function SignUpPage() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-50">
       <SignUp
+        routing="hash"
         appearance={{
           elements: {
             rootBox: "mx-auto",
